9e1a1e1ec415e1840bf733dfba2e9e52ac66e004
boscohyun <boscohyun@gmail.com>
2021-07-15 13:45:35 +0900

    Improve `PrepareGameCanvas`

---
+++ laylas-island/Assets/LaylasIsland/Frontend/Scripts/UI/Modules/PlayerPortrait.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using LaylasIsland.Frontend.Game;
+using LaylasIsland.Frontend.UI.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LaylasIsland.Frontend.UI.Modules
+{
+    public class PlayerPortrait : MonoBehaviour
+    {
+        // View
+
+        [SerializeField] private PortraitSpritesSO _spritesSo;
+        [SerializeField] private Image _portrait;
+
+        // ~View
+
+        public void Show(Player player)
+        {
+            // player.
+            var sprite = _spritesSo.Sprites.FirstOrDefault(e =>
+                e.name.Equals(player.portrait.Value));
+            if (!(sprite is null))
+            {
+                _portrait.overrideSprite = sprite;
+            }
+            
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
